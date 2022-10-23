ef2311f1b283071ba713c53a882aed084bc3fcd3
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-10-23 15:42:51 -0600

    added some signin functions

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import supabase from '$lib/db';
+
+	let email, password;
+
+	async function signIn() {
+		const { user, session, error } = await supabase.auth.signIn({
+			email,
+			password
+		});
+
+		if (error) alert(error.message);
+		console.log(user, session, error);
+	}
+</script>
+
+<form on:submit|preventDefault={signIn}>
+	<label for="email">Email</label>
+	<input class="blocl p-2 rounded" id="email" bind:value={email} type="email" />
+	<label for="password">Password</label>
+	<input class="blocl p-2 rounded" id="password" bind:value={password} type="password" />
+	<button class="p-2 bg-green-400 rounded block mx-auto">Sign In</button>
+</form>
+
+<a href="/auth/signup">Need to create a new account?</a>
