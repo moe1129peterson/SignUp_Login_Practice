e27a538c801ee4fec4e372406131ac04b580e2b0
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-10-22 21:09:08 -0600

    sign Up

---
+++ src/routes/+index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import supabase from '$lib/db';
+
+	async function signUp() {
+		const { user, error } = await supabase.auth.signUp({
+			email: 'moe1129tsuzuki@gmail.com',
+			password: '10938666'
+		});
+		console.log(user, error);
+	}
+</script>
+
+<button on:click={signUp}>Sign Up</button>
