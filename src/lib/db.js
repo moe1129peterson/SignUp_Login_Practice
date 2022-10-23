2a1eedb0273dfb3e0d1bc2d38fd5b7317cde1a86
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-10-22 22:45:16 -0600

    sign up is added

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lxzzcjvkcruocphnmvse.supabase.co';
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4enpjanZrY3J1b2NwaG5tdnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY0OTAzMDgsImV4cCI6MTk4MjA2NjMwOH0.f4bp2LbR7MxHwdZ_tn7i5I3HuqDK1DmjN4HDA3rMFP0';
+const supabase = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
