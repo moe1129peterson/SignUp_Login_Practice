32ec90eeb698ebc221caaedc3695467256038ed1
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-10-30 21:47:02 -0600

    decided to create shopping_app in this repo

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -7,17 +7,85 @@
 	}
 </script>
 
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
 <section class="text-gray-600 body-font">
-	<div class="container px-5 py-24 mx-auto">
-		<div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-			<h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
-				Sorry! This page is under constraction. Back to Log In Page.
-			</h1>
-			<button
-				on:click={navigate}
-				class="flex-shrink-0 text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg mt-10 sm:mt-0"
-				>Button</button
+	<div class="bg-white">
+		<div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+			<div
+				class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
 			>
+				<h1 class="sr-only">Products</h1>
+				<a href="#" class="group">
+					<div
+						class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+					>
+						<img
+							src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
+							alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+							class="h-full w-full object-cover object-center group-hover:opacity-75"
+						/>
+					</div>
+					<h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
+					<p class="mt-1 text-lg font-medium text-gray-900">$48</p>
+				</a>
+
+				<a href="#" class="group">
+					<div
+						class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+					>
+						<img
+							src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg"
+							alt="Olive drab green insulated bottle with flared screw lid and flat top."
+							class="h-full w-full object-cover object-center group-hover:opacity-75"
+						/>
+					</div>
+					<h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
+					<p class="mt-1 text-lg font-medium text-gray-900">$35</p>
+				</a>
+
+				<a href="#" class="group">
+					<div
+						class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+					>
+						<img
+							src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg"
+							alt="Person using a pen to cross a task off a productivity paper card."
+							class="h-full w-full object-cover object-center group-hover:opacity-75"
+						/>
+					</div>
+					<h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
+					<p class="mt-1 text-lg font-medium text-gray-900">$89</p>
+				</a>
+
+				<a href="#" class="group">
+					<div
+						class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+					>
+						<img
+							src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
+							alt="Hand holding black machined steel mechanical pencil with brass tip and top."
+							class="h-full w-full object-cover object-center group-hover:opacity-75"
+						/>
+					</div>
+					<h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+					<p class="mt-1 text-lg font-medium text-gray-900">$35</p>
+				</a>
+
+				<!-- More products... -->
+			</div>
 		</div>
 	</div>
 </section>
